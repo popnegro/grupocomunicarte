938baf5edb927bc52c8f7e2489dcf98142e6b880
popnegro <grasso.luis@gmail.com>
2026-08-12 03:08:53 -0300

    refactor: route firebase barrel through static auth core

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ export {
   signInWithRedirect,
   getRedirectResult,
   browserPopupRedirectResolver,
-} from "./firebase-auth";
+} from "./firebase-auth-core";
 export { db } from "./firebase-firestore";
 export { storage } from "./firebase-storage";
 export { appCheck } from "./firebase-app-check";

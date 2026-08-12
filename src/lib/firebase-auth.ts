97d28fdb8a58b3a74dfa53d50dfa0ee76586934b
popnegro <grasso.luis@gmail.com>
2026-08-12 03:07:13 -0300

    refactor: centralize firebase auth exports

--- src/lib/firebase-auth.ts
+++ src/lib/firebase-auth.ts
@@ -1,8 +1,11 @@
 import {
   getAuth,
   GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithRedirect,
+  signOut,
   getRedirectResult,
+  signInWithPopup,
   browserPopupRedirectResolver,
 } from "firebase/auth";
 import { app } from "./firebase-app";
@@ -10,13 +13,23 @@ import { app } from "./firebase-app";
 export const auth = getAuth(app);
 
 export const googleAuthProvider = new GoogleAuthProvider();
-
 googleAuthProvider.setCustomParameters({
   prompt: "select_account",
 });
 
+export const googleSlidesAuthProvider = new GoogleAuthProvider();
+googleSlidesAuthProvider.addScope("https://www.googleapis.com/auth/presentations");
+googleSlidesAuthProvider.addScope("https://www.googleapis.com/auth/drive");
+googleSlidesAuthProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
 export {
+  GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithRedirect,
+  signOut,
   getRedirectResult,
+  signInWithPopup,
   browserPopupRedirectResolver,
 };

633e993f791967dc185a83d9f7c4c8df6c00bbb1
popnegro <grasso.luis@gmail.com>
2026-08-15 23:15:30 -0300

    fix: isolate dashboard from public layout

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,26 @@ import Nosotros from './pages/Nosotros';
 import Dashboard from './pages/Dashboard';
 import { SelectionProvider } from './context/SelectionContext';
 
-function AnimatedRoutes() {
+function PublicRoutes() {
   const location = useLocation();
-  const isDashboard = location.pathname.startsWith('/dashboard');
+  return (
+    <Layout>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+          <Route path="/soportes" element={<PageTransition><Soportes /></PageTransition>} />
+          <Route path="/nosotros" element={<PageTransition><Nosotros /></PageTransition>} />
+          <Route path="/inventario" element={<PageTransition><Inventario /></PageTransition>} />
+        </Routes>
+      </AnimatePresence>
+    </Layout>
+  );
+}
 
-  if (isDashboard) {
+function AppRoutes() {
+  const location = useLocation();
+
+  if (location.pathname.startsWith('/dashboard')) {
     return (
       <Routes location={location}>
         <Route path="/dashboard" element={<Dashboard />} />
@@ -26,25 +41,14 @@ function AnimatedRoutes() {
     );
   }
 
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<PageTransition><Home /></PageTransition>} />
-        <Route path="/soportes" element={<PageTransition><Soportes /></PageTransition>} />
-        <Route path="/nosotros" element={<PageTransition><Nosotros /></PageTransition>} />
-        <Route path="/inventario" element={<PageTransition><Inventario /></PageTransition>} />
-      </Routes>
-    </AnimatePresence>
-  );
+  return <PublicRoutes />;
 }
 
 export default function App() {
   return (
     <Router>
       <SelectionProvider>
-        <Layout>
-          <AnimatedRoutes />
-        </Layout>
+        <AppRoutes />
       </SelectionProvider>
     </Router>
   );

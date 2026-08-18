22984b7e5182a9cbbfdaa3c600a1ff3c3189363d
popnegro <grasso.luis@gmail.com>
2026-08-17 22:11:07 -0300

    chore(pmv): remove temporary audit test script

--- audit/pmv-public-current/test-featured.ts
+++
@@ -1,5 +0,0 @@
-import { fixedLocations, mobileRoutes } from './src/data/inventory';
-const allItems = [...fixedLocations, ...mobileRoutes];
-const featuredItems = allItems.filter(item => item.isFeatured);
-console.log('Total items:', allItems.length);
-console.log('Featured items:', featuredItems.length);
